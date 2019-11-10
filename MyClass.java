f837d6d19c7c0dc01442ff8d8d7d55a5a0191251
Genie_Lab <rlawksml@gmail.com>
2019-11-10 16:34:27 +0900

    Funtion
    함수 사용 Test

---
+++ MyClass.java
@@ -0,0 +1,31 @@
+package Java_Nov;
+
+public class MyClass {
+	
+	
+	public void method1 () {
+		System.out.println("m1이 싱행됨 ");
+		
+	}
+	
+	public void method2 () {
+		System.out.println("m2가 실행됨");
+	}
+	
+	public int method3() {
+		System.out.println("m3가 실행됨");
+		return 3;
+	}
+	
+
+	public void method4(int x, int y) {
+		System.out.println(x + y +"m4가 실행됨");
+	}
+	
+	public int method5(int a) {
+		System.out.println(a + "m4가 실행됨");
+		return a * 2;
+	}
+	
+
+}
