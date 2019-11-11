ef0cae6a50491d3ce575ba78604133854493112f
Genie_Lab <rlawksml@gmail.com>
2019-11-11 22:55:02 +0900

    1111
    생성자 오버라이딩
    함수 오버라이딩

---
+++ MyClass2.java
@@ -0,0 +1,34 @@
+package Java_Nov;
+
+public class MyClass2 {
+
+	int age, grade;
+	String name;
+	
+	public MyClass2(int age, int grade, String name) {
+		this.age = age;
+		this.grade = grade;
+		this.name = name;
+		
+	}
+	
+	public MyClass2() {
+	}
+	
+	public int Plus(int x, int y, int z) {
+		return x + y + z;
+	}
+	public int Plus(int x, int y) {
+		return x + y;
+	}
+	public String Plus(String x, String y) {
+		return x + y;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+}
