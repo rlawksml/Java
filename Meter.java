90649c5752ca9a50185c4de0d5a394bcb73c7d8c
Genie_Lab <rlawksml@gmail.com>
2019-11-13 22:39:53 +0900

    Implements
    implements (abstract) override

---
+++ Meter.java
@@ -0,0 +1,6 @@
+package Java_Nov;
+
+public interface Meter {
+	public abstract void start();
+	public abstract int stop(int distance);
+}
---
+++ Taxi.java
@@ -0,0 +1,18 @@
+package Java_Nov;
+
+public class Taxi implements Meter {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int stop(int distance) {
+		// TODO Auto-generated method stub
+		return 0;
+		
+	}
+
+}
